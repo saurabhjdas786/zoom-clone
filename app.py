278d00dc0b9772d36deacfe41aad879cd4818170
saurabhjdas786 <saurabhjdas786@gmail.com>
2024-04-14 22:48:13 -0300

    add comments

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ public_ip_addr = requests.get("https://api.ipify.org").text
 print(local_ip_addr)
 print(public_ip_addr)
 
-server = StreamingServer(local_ip_addr, 9999)
-receiver = AudioReceiver(local_ip_addr, 8888)
+server = StreamingServer(local_ip_addr, 9999) # send
+receiver = AudioReceiver(local_ip_addr, 8888) # receive
 
 #adding function for connecting two servers
 def start_listening():
@@ -22,19 +22,19 @@ def start_listening():
 
 #adding function for video
 def start_camera_stream():
-    camera_client = CameraClient(text_target_ip.get(1.0,'end-1c'), 7777)
+    camera_client = CameraClient(text_target_ip.get(1.0,'end-1c'), 7777) # receive
     t3 = threading.Thread(target = camera_client.start_stream)
     t3.start()
 
 #adding function for screen sharing
 def start_screen_sharing():
-    screen_client = ScreenShareClient(text_target_ip.get(1.0,'end-1c'), 7777)
+    screen_client = ScreenShareClient(text_target_ip.get(1.0,'end-1c'), 7777) # receive
     t4 = threading.Thread(target = screen_client.start_stream)
     t4.start()
 
 #adding function for audio streaming
 def start_audio_stream():
-    audio_sender = AudioSender(text_target_ip.get(1.0,'end-1c'), 6666)
+    audio_sender = AudioSender(text_target_ip.get(1.0,'end-1c'), 6666) # send
     t5 = threading.Thread(target = audio_sender.start_stream)
     t5.start()
 
