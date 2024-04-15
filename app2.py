9867c271ca148d70b86b4eb7d1ed87ab5288220a
saurabhjdas786 <saurabhjdas786@gmail.com>
2024-04-14 22:42:20 -0300

    working

--- app2.py
+++ app2.py
@@ -10,8 +10,8 @@ public_ip_addr = requests.get("https://api.ipify.org").text
 print(local_ip_addr)
 print(public_ip_addr)
 
-server = StreamingServer(local_ip_addr, 9999)
-receiver = AudioReceiver(local_ip_addr, 8888)
+server = StreamingServer(local_ip_addr, 7777)
+receiver = AudioReceiver(local_ip_addr, 6666)
 
 #adding function for connecting two servers
 def start_listening():
@@ -22,25 +22,25 @@ def start_listening():
 
 #adding function for video
 def start_camera_stream():
-    camera_client = CameraClient(text_target_ip.get(1.0,'end-1c'), 7777)
+    camera_client = CameraClient(text_target_ip.get(1.0,'end-1c'), 9999)
     t3 = threading.Thread(target = camera_client.start_stream)
     t3.start()
 
 #adding function for screen sharing
 def start_screen_sharing():
-    screen_client = ScreenShareClient(text_target_ip.get(1.0,'end-1c'), 7777)
+    screen_client = ScreenShareClient(text_target_ip.get(1.0,'end-1c'), 9999)
     t4 = threading.Thread(target = screen_client.start_stream)
     t4.start()
 
 #adding function for audio streaming
 def start_audio_stream():
-    audio_sender = AudioSender(text_target_ip.get(1.0,'end-1c'), 6666)
+    audio_sender = AudioSender(text_target_ip.get(1.0,'end-1c'), 8888)
     t5 = threading.Thread(target = audio_sender.start_stream)
     t5.start()
 
 
 window = tk.Tk()
-window.title("My zoom app - streamer")
+window.title("My zoom app - client")
 window.geometry("350x250")
 
 label_target_ip = tk.Label(window, text="Target IP:")
